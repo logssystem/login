5d3e14b5efe496640ca5b94b9ece7925f30d049f
logssystem <joaorodrigue0@gmail.com>
2026-05-23 16:50:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('input[type="text"], input[type="email"]');
+
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    if(input.value.length > 0){
+      input.style.borderColor = '#000';
+    } else {
+      input.style.borderColor = '#ccc';
+    }
+  });
+});
